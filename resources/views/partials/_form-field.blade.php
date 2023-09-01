4f2bb61658fc68981db9b0f0fe13467d58829a39
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-09-01 14:23:51 -0700

    refactored form

---
+++ resources/views/partials/_form-field.blade.php
@@ -0,0 +1,16 @@
+@props(['name', 'label', 'type' => 'text', 'placeholder' => '', 'rows' => 3])
+
+<div class="mb-6">
+    <label for="{{ $name }}" class="inline-block text-lg mb-2">{{ $label }}</label>
+    @if ($type === 'textarea')
+        <textarea class="border border-gray-200 rounded p-2 w-full" name="{{ $name }}" rows="{{ $rows }}"
+            placeholder="{{ $placeholder }}">{{ old($name) }}</textarea>
+    @else
+        <input type="{{ $type }}" class="border border-gray-200 rounded p-2 w-full" name="{{ $name }}"
+            placeholder="{{ $placeholder }}" value="{{ old($name) }}">
+    @endif
+
+    @error($name)
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+    @enderror
+</div>
