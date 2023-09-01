7e6a6967cb6f9e6f1480cf8662e885f66bd07f8a
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-09-01 13:26:44 -0700

    Added filter functionality

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,16 +8,18 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index(){
-        dd(request('tag'));
+    public function index()
+    {
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
-    public function show(Listing $listing) {
+    public function show(Listing $listing)
+    {
         return view('listings.show', [
             'listing' => $listing
         ]);
     }
 }
+
